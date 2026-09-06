9a89447b6c1fe25f1d8ea0f38aa2348df5d2d0c8
Rui Fernando <rui.fernando@aluno.uepb.edu.br>
2026-09-06 02:58:32 -0300

    feat: implementa entidade Pedido, datas com Instant e padrão ISO 8601 / parte 2

--- src/main/java/com/educandoweb/course/entities/Order.java
+++ src/main/java/com/educandoweb/course/entities/Order.java
@@ -1,9 +1,11 @@
 package com.educandoweb.course.entities;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import jakarta.persistence.*;
 
 import java.io.Serializable;
 import java.time.Instant;
+import java.util.regex.Pattern;
 
 @Entity
 @Table(name = "tb_order")
@@ -14,8 +16,11 @@ public class Order implements Serializable {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern="yyyy-MM-dd'T'HH:mm:ss'Z'", timezone = "GMT")
     private Instant moment;
     //private OrderStatus orderStatus;
+
 
     @ManyToOne //n para n
     @JoinColumn(name = "client_id")
