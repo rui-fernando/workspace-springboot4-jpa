8e09727a0c8a56b256ca86ff3cba5b2ab8c7cf41
Rui Fernando <rui.fernando@aluno.uepb.edu.br>
2026-09-06 14:30:09 -0300

    feat: implementa entidade OrdemItem / parte 2

--- src/main/java/com/educandoweb/course/entities/Order.java
+++ src/main/java/com/educandoweb/course/entities/Order.java
@@ -6,6 +6,8 @@ import jakarta.persistence.*;
 
 import java.io.Serializable;
 import java.time.Instant;
+import java.util.HashSet;
+import java.util.Set;
 
 
 @Entity
@@ -27,6 +29,9 @@ public class Order implements Serializable {
     @ManyToOne //n para n
     @JoinColumn(name = "client_id")
     private User client;
+
+    @OneToMany(mappedBy = "id.order")
+    private Set<OrderItem> items = new HashSet<>();
 
     public Order(){
     }
@@ -69,6 +74,10 @@ public class Order implements Serializable {
         if (orderStatus != null) {
             this.orderStatus = orderStatus.getCode();
         }
+    }
+
+    public Set<OrderItem> getItems() {
+        return items;
     }
 
     @Override
--- src/main/java/com/educandoweb/course/entities/User.java
+++ src/main/java/com/educandoweb/course/entities/User.java
@@ -21,7 +21,7 @@ public class User implements Serializable {
     private String phone;
     private String password;
 
-    @JsonIgnore
+    @JsonIgnore //impedir associação de mão dupla
     @OneToMany(mappedBy = "client")
     private List<Order> orders = new ArrayList<>();
 
